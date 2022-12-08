ebf44bb1d50a89bbc0aaa4583aec500b548deff8
Mohammad Hossein Heidary <mohammadhsein9@gmail.com>
2022-12-08 20:33:28 +0330

    Sign up completed

--- log/__init__.py
+++ log/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '2493020c05bc64c07f6bedc8e21c9fc4'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../users.db'
 app.app_context().push()
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from log import routes
--- log/routes.py
+++ log/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template
-from log import app
+from flask import render_template, redirect, url_for
+from log import app, db, bcrypt
 from log.forms import SignUpForm, LoginForm
+from log.models import User
 
 
 @app.route('/')
@@ -8,7 +9,19 @@ def home():
     return render_template('home.html')
 
 
-@app.route('/register')
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = SignUpForm()
-    return render_template('SignUp.html', form = form)
+    if form.validate_on_submit():
+        hashed_pass = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_pass)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template('SignUp.html', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', form=form)
